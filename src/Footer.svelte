9024065296e91a308599c361bf5cc5fe551b7cf9
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-01-17 18:33:37 +0400

    fist commit

---
+++ src/Footer.svelte
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      Simple TodoApp with
+      <strong>Svelte 3</strong>
+      and
+      <strong>Bulma CSS</strong>
+    </p>
+  </div>
+</footer>
